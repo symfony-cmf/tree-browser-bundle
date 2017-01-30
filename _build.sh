096b32dcc4c3fe070b1d7e85542b2135d04b1f0c
WouterJ <waldio.webdesign@gmail.com>
2017-01-30 20:34:44 +0100

    Rebuild assets

--- _build.sh
+++ _build.sh
@@ -12,13 +12,13 @@ webpack -p --progress
 echo ""
 echo " * Copying required vendor files to public directory..."
 # jQuery
-if [ ! -d "./Resources/public/vendor/jquery" ]; then
-    mkdir -p ./Resources/public/vendor/jquery/dist
+if [ ! -d "./src/Resources/public/vendor/jquery" ]; then
+    mkdir -p ./src/Resources/public/vendor/jquery/dist
 fi
-cp ./bower_components/jquery/dist/jquery.min.js ./Resources/public/vendor/jquery/dist/jquery.min.js
+cp ./bower_components/jquery/dist/jquery.min.js ./src/Resources/public/vendor/jquery/dist/jquery.min.js
 
 # jQuery UI
 if [ ! -d "./Resources/public/vendor/jquery-ui" ]; then
     mkdir ./Resources/public/vendor/jquery-ui
 fi
-cp ./bower_components/jquery-ui/jquery-ui.min.js ./Resources/public/vendor/jquery-ui/jquery-ui.min.js
+cp ./bower_components/jquery-ui/jquery-ui.min.js ./src/Resources/public/vendor/jquery-ui/jquery-ui.min.js
